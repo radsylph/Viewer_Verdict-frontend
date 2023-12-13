183d7c9cd09813d5f0744a1801cf26f6da78a702
radsylph <andresa@devtop.io>
2023-12-12 22:22:42 -0400

    feat: changed overview property to optional

--- src/app/interfaces/serie.interface.ts
+++ src/app/interfaces/serie.interface.ts
@@ -1,17 +1,16 @@
 export interface SerieInterface {
-    idApi: number;
-    name: string;
-    overview: string;
-    tagline: string;
-    posters: string[];
-    firstAir: string;
-    lastAir: string;
-    totalEpisodes: number;
-    totalSeasons: number;
-    genres: string[];
-    trailers: string[];
-    status: string;
-    voteAverage?: number;
-    voteCount?: number;
-  }
-  
+  idApi: number;
+  name: string;
+  overview?: string;
+  tagline: string;
+  posters: string[];
+  firstAir: string;
+  lastAir: string;
+  totalEpisodes: number;
+  totalSeasons: number;
+  genres: string[];
+  trailers: string[];
+  status: string;
+  voteAverage?: number;
+  voteCount?: number;
+}
