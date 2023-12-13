6f9378476208060a9c6f7679a2be4c306d410851
radsylph <andresa@devtop.io>
2023-12-12 22:23:43 -0400

    feat: added loading page, new styles and functionality

--- src/app/tabs/scream-movies/scream-movies.module.ts
+++ src/app/tabs/scream-movies/scream-movies.module.ts
@@ -8,13 +8,16 @@ import { ScreamMoviesPageRoutingModule } from './scream-movies-routing.module';
 
 import { ScreamMoviesPage } from './scream-movies.page';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ScreamMoviesPageRoutingModule
+    ScreamMoviesPageRoutingModule,
   ],
-  declarations: [ScreamMoviesPage]
+  declarations: [ScreamMoviesPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ScreamMoviesPageModule {}
