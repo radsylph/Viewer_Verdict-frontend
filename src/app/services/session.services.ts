93ad709776da76798187ef6b5a3aa1830f0aa642
radsylph <andresa@devtop.io>
2023-12-13 21:22:38 -0400

    feat: created new service

---
+++ src/app/services/session.services.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { newUser, ErrorInterface, existingUser } from '../interfaces/main';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class SessionService {
+  constructor(private http: HttpClient) {}
+
+  private newUser: newUser = {
+    name: '',
+    lastname: '',
+    username: '',
+    email: '',
+    password: '',
+    repeat_password: '',
+    captchaResponse: undefined,
+    profilePicture: 'image.png',
+  };
+  private existingUser = {
+    user_info: '',
+    password: '',
+  };
+
+  private BackenUrl: string =
+    'http://localhost:7338' ||
+    'https://viewerverdict-backend-production.up.railway.app';
+
+  createUser(newUser: newUser) {
+    return this.http.post(`${this.BackenUrl}/auth/create`, newUser).pipe(
+      catchError((error) => {
+        console.log(error);
+        return throwError(error);
+      })
+    );
+  }
+
+  loginUser(existingUser: existingUser) {
+    return this.http.post(`${this.BackenUrl}/auth/login`, existingUser).pipe(
+      catchError((error) => {
+        console.log(error);
+        return throwError(error);
+      })
+    );
+  }
+
+    resetPassword(email: string) {
+        return this.http
+        .post(`${this.BackenUrl}/auth/reset_password`, { email })
+        .pipe(
+            catchError((error) => {
+            console.log(error);
+            return throwError(error);
+            })
+        );
+    }
+}
