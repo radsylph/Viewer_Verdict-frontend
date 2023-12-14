3ff2cfd0cd7a5aab4b1363375a2345cca2ad997b
radsylph <andresa@devtop.io>
2023-12-14 02:13:11 -0400

    feat: new methods and styles for reviewing a series

--- src/app/tabs/scream-series/scream-series.page.ts
+++ src/app/tabs/scream-series/scream-series.page.ts
@@ -3,10 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import { NavController } from '@ionic/angular';
 import { MovieService } from '../../services/movie.services';
 import { Preferences } from '@capacitor/preferences';
-import { SerieInterface } from 'src/app/interfaces/main';
+import { SerieInterface, ReviewInterface } from 'src/app/interfaces/main';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ModalController } from '@ionic/angular';
 import { ViewWillEnter } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-scream-series',
@@ -14,6 +16,16 @@ import { ViewWillEnter } from '@ionic/angular';
   styleUrls: ['./scream-series.page.scss'],
 })
 export class ScreamSeriesPage implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private navCtrl: NavController,
+    private ms: MovieService,
+    private sanitizer: DomSanitizer,
+    public modalController: ModalController,
+    private message: ToastController,
+    private alert: AlertController
+  ) {}
+
   items: any = [];
   itemsVideo: any = [];
   itemsSeasons: any = [];
@@ -35,18 +47,37 @@ export class ScreamSeriesPage implements OnInit {
     voteAverage: 0,
     voteCount: 0,
   };
-  constructor(
-    private http: HttpClient,
-    private navCtrl: NavController,
-    private ms: MovieService,
-    private sanitizer: DomSanitizer
-  ) {}
+
+  public review: ReviewInterface = {
+    owner: '',
+    mediaId: '',
+    rating: 0,
+    review: '',
+    edited: false,
+    type: '',
+    isComment: false,
+  };
+
+  public audienceReviews: any = [];
+  public criticReviews: any = [];
+  public data: any = 0;
+  public hasSession: boolean = false;
 
   handleRefresh(event: any) {
     setTimeout(() => {
       this.ionViewWillEnter();
       event.target.complete();
     }, 2000);
+  }
+
+  async getToken() {
+    const token = await Preferences.get({ key: 'token' });
+    console.log(token);
+    if (token.value) {
+      return (this.hasSession = true);
+    } else {
+      return (this.hasSession = false);
+    }
   }
 
   async ionViewWillEnter() {
@@ -70,39 +101,36 @@ export class ScreamSeriesPage implements OnInit {
     await Preferences.get({ key: 'serieId' }).then((data: any) => {
       this.serieId = data.value;
     });
-    console.log(this.serieId);
+    await this.getToken();
     const serieFromDb = await this.ms.getSerieDetails(this.serieId);
-
+    console.log(serieFromDb);
     this.serieDetails = serieFromDb.serie;
-    console.log(this.serieDetails);
+    this.criticReviews = serieFromDb.criticReviews;
+    this.audienceReviews = serieFromDb.publicReviews;
     console.log('test');
     await setTimeout(() => {
       this.isloading = true;
     }, 2000);
   }
 
+  async createMessage(Message: string) {
+    const newMessage = await this.message.create({
+      message: Message,
+      duration: 2000,
+      position: 'bottom',
+    });
+    await newMessage.present();
+  }
+
   async gotToDiscover() {
     this.navCtrl.navigateBack('/tabs/discover');
   }
-  ngOnInit() {
-    for (let i = 1; i < 9; i++) {
-      this.items.push(`${i}`);
-    }
-    for (let i = 1; i < 4; i++) {
-      this.itemsVideo.push(`${i}`);
-    }
-    for (let i = 1; i < 7; i++) {
-      this.itemsSeasons.push(`${i}`);
-    }
-    console.log(this.data);
-  }
+  ngOnInit() {}
 
   stars: number[] = [1, 2, 3, 4, 5];
 
   selectedValue: number = 0;
   isMouseover = true;
-
-  public data: any;
 
   //control del modal
   isModalRating = false;
@@ -118,7 +146,13 @@ export class ScreamSeriesPage implements OnInit {
   }
 
   setOpenRating(isOpen: boolean) {
-    this.isModalRating = isOpen;
+    console.log(this.hasSession);
+    if (this.hasSession === true) {
+      this.isModalRating = isOpen;
+    } else if (this.hasSession === false) {
+      console.log('no hay sesion');
+      this.createMessage('You need to be logged in to rate a movie');
+    }
   }
 
   getVideoUrl2(url: string): SafeResourceUrl {
@@ -127,15 +161,43 @@ export class ScreamSeriesPage implements OnInit {
     return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
   }
 
-  //counting stars
+  async createReview() {
+    this.review.mediaId = this.serieId;
+    console.log(this.review);
+    if (this.review.review == '' || this.review.rating === 0) {
+      this.alert
+        .create({
+          header: 'Error',
+          message: 'Please fill all the fields',
+          buttons: ['OK'],
+        })
+        .then((alert) => alert.present());
+      return;
+    }
+    try {
+      await this.ms.reviewSerie(this.review, this.serieId);
+      this.createMessage('Review created successfully');
+      this.ionViewWillEnter();
+      this.setOpenRating(false);
+    } catch (error: any) {
+      console.log(error);
+      this.alert
+        .create({
+          header: 'Error',
+          message: error.error.msg,
+          buttons: ['OK'],
+        })
+        .then((alert) => alert.present());
+    }
+  }
+
   countStar(star: number) {
     this.isMouseover = false;
     this.selectedValue = star;
     this.data = this.selectedValue;
     console.log(this.data);
+    this.review.rating = this.data;
   }
-
-  //for adding star
 
   addClass(star: number) {
     if (this.isMouseover) {
@@ -144,7 +206,6 @@ export class ScreamSeriesPage implements OnInit {
     }
   }
 
-  //for removing star
   removeClass() {
     if (this.isMouseover) {
       this.selectedValue = 0;
