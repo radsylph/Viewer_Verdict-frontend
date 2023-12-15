4e1d0bb4799daa60bff44952de0291d417bd9cd5
radsylph <andresa@devtop.io>
2023-12-15 16:19:55 -0400

    feat: added changes to the media interface

--- src/app/interfaces/media.interface.ts
+++ src/app/interfaces/media.interface.ts
@@ -1,10 +1,11 @@
 export interface MediaInterface {
-    title?: string;
-    name?: string;
-    overview: string;
-    poster: string;
-    type?: string;
-    idApi: number;
-    genres?: string[];
-  }
-  
+  title?: string;
+  name?: string;
+  overview?: string;
+  poster: string;
+  type?: string;
+  releaseDate?: string;
+  popularity?: number;
+  idApi: number;
+  genres?: string[];
+}
