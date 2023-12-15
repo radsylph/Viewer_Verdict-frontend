9771244d5082d75ece453720a1bbc04a3030f095
radsylph <andresa@devtop.io>
2023-12-15 01:54:17 -0400

    feat: added new properties

--- src/app/interfaces/review.interface.ts
+++ src/app/interfaces/review.interface.ts
@@ -1,10 +1,12 @@
 export interface ReviewInterface {
   owner: string;
   mediaId: string;
-  rating: number;
+  rating?: number;
   review?: string;
   edited?: boolean;
   type: string;
   createdAt?: Date;
   isComment?: boolean;
+  replyTo?: any;
+  id?: number;
 }
