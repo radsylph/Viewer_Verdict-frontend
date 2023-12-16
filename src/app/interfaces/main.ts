33ca28460771b2c1a872fd52c66015a01ce52a58
radsylph <andresa@devtop.io>
2023-12-16 04:46:52 -0400

    feat: added imports

--- src/app/interfaces/main.ts
+++ src/app/interfaces/main.ts
@@ -5,6 +5,8 @@ import { GenreInterface } from './genres.interface';
 import { ErrorInterface } from './error.interface';
 import { existingUser, newUser, userProfile } from './user.interface';
 import { ReviewInterface } from './review.interface';
+import { ChatInterface } from './chat.interface';
+import { MessageInterface } from './message.interface';
 
 export {
   MediaInterface,
@@ -16,4 +18,6 @@ export {
   newUser,
   userProfile,
   ReviewInterface,
+  ChatInterface,
+  MessageInterface,
 };
